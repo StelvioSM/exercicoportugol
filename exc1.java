2260a3c053174fdfd6472491214403a8711f4287
Stelvio Alexandre Smith <silviosmith18@gmail.com>
2021-07-05 22:51:25 -0300

    exercicos de repeticao

---
+++ exc1.java
@@ -0,0 +1,19 @@
+package PrimeiroExercicio;
+
+public class exc1 {
+	public static void main(String[] args) { 
+		int x;
+	    for(x=1000;x<2000;x++) {
+		 if(x%11==5) {
+			 System.out.println(x+"dividido por 11 resulta em 5.");
+			 
+			 
+		 }
+		
+	}
+ 
+	 
+ }
+	
+	}
+
